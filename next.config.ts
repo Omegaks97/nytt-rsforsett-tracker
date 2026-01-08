46908fc116b0e06b5bb05b6d4f519078403ce890
Omegaks97 <jorgen-skagemo@hotmail.com>
2026-01-08 19:34:13 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Vi legger ikke til noe spesielt her nå, 
+  // men ved å ha en ren fil sikrer vi at ingen gamle Turbopack-innstillinger ligger og spøker.
 };
 
 export default nextConfig;

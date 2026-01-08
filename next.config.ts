9f3645ed6946974a94a01a208d67c4dcc43821b5
Omegaks97 <jorgen-skagemo@hotmail.com>
2026-01-08 19:54:45 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // Vi legger ikke til noe spesielt her nå, 
-  // men ved å ha en ren fil sikrer vi at ingen gamle Turbopack-innstillinger ligger og spøker.
+  experimental: {
+    optimizePackageImports: ["tailwindcss"],
+  },
 };
 
 export default nextConfig;

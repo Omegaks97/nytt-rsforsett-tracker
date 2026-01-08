518864069f906274da0bd0d17ec85db0a7faa284
Omegaks97 <jorgen-skagemo@hotmail.com>
2026-01-08 19:59:26 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    optimizePackageImports: ["tailwindcss"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
